844c0f24957db14e2b5bfa5c2305afaec974a33c
Joost Aarts <joostaarts@hotmail.com>
2018-06-17 17:57:51 +0200

    Remove readkey

--- cmd/main.go
+++ cmd/main.go
@@ -1,21 +1,18 @@
 package main
 
 import (
-	"log"
-	"os"
+	"time"
 
 	"github.com/joostaarts/GolangHue/pkg/bridgediscovery"
 )
 
 func main() {
 	bridgediscovery.StartDiscovery()
-	log.Println("press a key")
-	waitForKeyPress()
+	sleep()
 }
 
-func waitForKeyPress() {
-	b := make([]byte, 10)
-	if _, err := os.Stdin.Read(b); err != nil {
-		log.Fatal(err)
+func sleep() {
+	for {
+		time.Sleep(time.Second)
 	}
 }
